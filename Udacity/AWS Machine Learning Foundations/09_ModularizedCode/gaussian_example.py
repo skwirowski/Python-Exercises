f433cf47471bcb2f7e60216c243792f03a0c580d
Paweł Skwirowski <pskwirowski@gmail.com>
2020-07-14 08:25:38 +0200

    Add work on modularazing and packaging code
      - add modularized Gaussian distribution code
      - created Gaussian distribution code package
      - start working on Binomial distribution class

---
+++ Udacity/AWS Machine Learning Foundations/09_ModularizedCode/gaussian_example.py
@@ -0,0 +1,5 @@
+from gaussian_distribution import Gaussian
+
+gaussian_one = Gaussian(22, 3)
+print(gaussian_one.mean)
+print(gaussian_one.stdev)
---
+++ Udacity/AWS Machine Learning Foundations/10_CodePackage/gaussian_example.py
@@ -0,0 +1,5 @@
+from distributions import Gaussian
+
+gaussian_one = Gaussian(22, 3)
+print(gaussian_one.mean)
+print(gaussian_one.stdev)
